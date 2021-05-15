87ea5aebe243bad80e52e24a30f4b4f1fd22af54
Vaishali Sharma <33980820+vaishali161197@users.noreply.github.com>
2021-05-15 22:55:57 +0530

    Load scene on collision

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
-     void OnCollisionEnter(Collision other)
-    {
-        Debug.Log(this.name + "--collidedWith--" + other.gameObject.name);
-    }
-    private void OnTriggerEnter(Collider other)
+    [SerializeField] float loadDelay = 1f;
+     private void OnTriggerEnter(Collider other)
     {
         Debug.Log($"{this.name} **triggered by** {other.gameObject.name}");
+        StartCrashSequence();
+    }
+
+    void StartCrashSequence()
+    {
+        GetComponent<PlayerController>().enabled = false;
+        Invoke("ReloadLevel", loadDelay);
+    }
+
+    private void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;   
+         SceneManager.LoadScene(currentSceneIndex);
     }
 }
