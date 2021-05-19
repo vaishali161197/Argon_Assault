959f7cbac81491625e0b42f1723b502aebf8b8a0
Vaishali Sharma <33980820+vaishali161197@users.noreply.github.com>
2021-05-19 19:04:52 +0530

    Explosion after collision

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] float loadDelay = 1f;
+    [SerializeField] ParticleSystem crashVFX;
      private void OnTriggerEnter(Collider other)
     {
         Debug.Log($"{this.name} **triggered by** {other.gameObject.name}");
@@ -13,6 +14,9 @@ public class CollisionHandler : MonoBehaviour
 
     void StartCrashSequence()
     {
+        crashVFX.Play();
+        GetComponent<BoxCollider>().enabled = false;
+        GetComponent<MeshRenderer>().enabled = false;
         GetComponent<PlayerController>().enabled = false;
         Invoke("ReloadLevel", loadDelay);
     }
