4e32002c6f27fd9ae8f2d54a3f6c193cc2d43bcb
Vaishali Sharma <33980820+vaishali161197@users.noreply.github.com>
2021-05-21 17:39:15 +0530

    Increase score per kill

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,11 +6,28 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform spawnAtRunTime;
+    [SerializeField] int scorePerHit = 15;
+    ScoreBoard scoreBoard;
+    private void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
     private void OnParticleCollision(GameObject other)
+    {
+        processHit();
+        killEnemy();
+       
+    }
+    private void processHit()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
+        
+    }
+    private void killEnemy()
     {
         GameObject VFX = Instantiate(deathVFX, transform.position, Quaternion.identity);
         VFX.transform.parent = spawnAtRunTime;
-
         Destroy(gameObject);
     }
 }
+
