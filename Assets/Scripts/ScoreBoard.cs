fcf4681cd5efd9c5618049fcbdbb35b38fd0eaa0
Vaishali Sharma <33980820+vaishali161197@users.noreply.github.com>
2021-05-21 21:38:57 +0530

    using Tostring()

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class ScoreBoard : MonoBehaviour
 {
     int score;
+    TextMeshProUGUI scoreText;
+
+    private void Start()
+    {
+        scoreText = GetComponent<TextMeshProUGUI>();
+        scoreText.text = score.ToString();
+    }
     public void IncreaseScore(int AmountToIncrease)
     {
         score += AmountToIncrease;
-        Debug.Log("score is : "+ score);
+        scoreText.text = score.ToString();
     }
 
     
