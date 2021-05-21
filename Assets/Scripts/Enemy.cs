c713a03d04485a277013ee020da6897da9fa76f6
Vaishali Sharma <33980820+vaishali161197@users.noreply.github.com>
2021-05-21 22:04:33 +0530

    Add hitpoints for enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVFX;
+    [SerializeField] int hitPoint = 2;
     [SerializeField] Transform spawnAtRunTime;
     [SerializeField] int scorePerHit = 15;
     ScoreBoard scoreBoard;
@@ -15,11 +17,17 @@ public class Enemy : MonoBehaviour
     private void OnParticleCollision(GameObject other)
     {
         processHit();
-        killEnemy();
+        if(hitPoint < 1)
+        {
+            killEnemy();
+        }
+
+       
        
     }
     private void processHit()
     {
+        hitPoint--;
         scoreBoard.IncreaseScore(scorePerHit);
         
     }
